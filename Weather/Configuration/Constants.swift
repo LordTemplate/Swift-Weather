2a560fc9028da1b11400133a23de340b2cde749d
Alex Hernandez <ivan.hdez1984@gmail.com>
2023-05-19 06:41:26 -0600

    Weather

---
+++ Weather/Configuration/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Ivan Alejandro Hernandez Sanchez on 18/05/23.
+//
+
+import Foundation
+
+class Constants {
+    /// My ApyKey
+    static let ApiKey = "6d7e2005b43d445a176a4d8d2097a10c"
+}
