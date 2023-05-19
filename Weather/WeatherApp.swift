69fe3130feb8c91d2b0c825627a3a7e20e21258b
Alex Hernandez <ivan.hdez1984@gmail.com>
2023-05-18 21:55:37 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ivan Alejandro Hernandez Sanchez on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
